52bd7aeccabb6107907458a81ccc724746347218
d3nw3r <d3nw3r@gmail.com>
2021-10-18 21:28:05 +0300

    add TITLE page, and change index.html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    if util.get_entry(title):
+        return render(request, "encyclopedia/title.html", {
+            'title_name': title,
+            'title_context': util.get_entry(title)
+        })
+    else:
+        return render(request, "encyclopedia/none_title.html", {
+            'title_name': title
+        })
+
