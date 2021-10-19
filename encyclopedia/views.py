1655e25d2f55f137a702a3b815ac7c7d2466754c
d3nw3r <d3nw3r@gmail.com>
2021-10-19 14:01:51 +0300

    add function search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
+import re
+
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def title(request, title):
     if util.get_entry(title):
@@ -19,3 +22,23 @@ def title(request, title):
             'title_name': title
         })
 
+
+# функція пошуку на сторінці
+def search(request):
+    found_matches = []
+    if request.method == 'GET':
+        form = request.GET
+    list_of_articles = [item.lower() for item in util.list_entries()]
+    if form['q'] in list_of_articles:
+        return title(request, form['q'])
+    elif list_of_articles:
+        for article in list_of_articles:
+            if re.findall(form['q'].lower(), article):
+                found_matches.append(article)
+        return render(request, "encyclopedia/search.html", {
+            'contents': found_matches
+        })
+    else:
+        return render(request, "encyclopedia/search.html", {
+            'search_content': form['q']
+        })
