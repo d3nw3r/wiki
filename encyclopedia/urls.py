c1202b729b4a14c91bf0caf9f234fca3b922eab9
d3nw3r <d3nw3r@gmail.com>
2021-10-27 19:26:18 +0300

    last

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'wiki'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:title>", views.title, name='title'),
-    path("search/", views.search, name='search'),
-    path("randompage/", views.random_choice, name='random_choice'),
-    path("createpage/", views.create_page, name='create_page'),
-    path("edit/<str:title>", views.edit, name='edit'),
-    path("saveedit/", views.saveedit, name='save_edit')
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:title>", views.title, name='title'),
+    path("wiki/search/", views.search, name='search'),
+    path("wiki/randompage/", views.random_choice, name='random_choice'),
+    path("wiki/createpage/", views.create_page, name='create_page'),
+    path("wiki/edit/<str:title>", views.edit, name='edit'),
+    path("wiki/saveedit/", views.saveedit, name='save_edit')
     ]
